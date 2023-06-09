2c2b02ad52e5be64c2b529984558f82cf7c44258
Deren B <dbozerprogramming@gmail.com>
2023-06-09 18:02:55 -0400

    App Settings

--- AppSettingsCourse/BasicDemo/Program.cs
+++ AppSettingsCourse/BasicDemo/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.Configure<EmailSettingsOptions>(Configuration.GetSection("EmailSettings"));
+
 
 var app = builder.Build();
 
