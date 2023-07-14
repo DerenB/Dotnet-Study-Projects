a34a12e8851e40bb596af41c36e5358300aeb427
Deren B <dbozerprogramming@gmail.com>
2023-07-14 19:42:50 -0400

    Rebuilding Project

--- Blazor-DataAccess/DataAccessDemo/SampleDB/dbo/Tables/People.sql
+++ Blazor-DataAccess/DataAccessDemo/SampleDB/dbo/Tables/People.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[People]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
-	[FirstName] NVARCHAR(50) NULL, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NULL, 
     [LastName] NVARCHAR(50) NULL, 
-    [DateOfBirth] DATETIME2 NULL,
+    [DateOfBirth] DATETIME2 NULL
 )
