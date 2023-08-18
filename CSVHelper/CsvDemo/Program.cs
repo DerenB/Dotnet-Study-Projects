905ce00ba5bbb74558fd1e115f1e2607c6ede859
Deren B <47374311+DerenB@users.noreply.github.com>
2023-08-18 00:49:49 -0400

    CSV Helper Demo

--- CSVHelper/CsvDemo/Program.cs
+++ CSVHelper/CsvDemo/Program.cs
@@ -2,16 +2,43 @@
 using CsvDemo;
 using CsvHelper;
 
-string inputFile = @"C:\Users\dboze\Documents\LocalRepo\Dotnet\Dotnet-Study-Projects\CSVHelper\people-100.csv";
-string outputFile = @"C:\Users\dboze\Documents\LocalRepo\Dotnet\Dotnet-Study-Projects\CSVHelper\filtered-people.csv";
+namespace CsvDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            string inputFile = @"C:\Users\dboze\Documents\LocalRepo\Dotnet\Dotnet-Study-Projects\CSVHelper\people-100.csv";
+            string outputFile = @"C:\Users\dboze\Documents\LocalRepo\Dotnet\Dotnet-Study-Projects\CSVHelper\filtered-people.csv";
+            List<PersonModel> outputRecords = new();
 
-List<PersonModel> outputRecords = new();
+            // Create a stream connection to the CSV file
+            using var reader = new StreamReader(inputFile);
 
-// Read CSV file
-using var reader = new StreamReader(inputFile);
-using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            // Reads the CSV
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
 
+            // Gets the records
+            var records = csv.GetRecords<PersonModel>();
 
+            foreach (var record in records)
+            {
+                if(record.FirstName.StartsWith("S"))
+                {
+                    outputRecords.Add(record);
+                }
+                //Console.WriteLine(record.FirstName + " " + record.LastName);
+            }
+            Console.WriteLine($"Number of records: {outputRecords.Count}.");
+
+            // Output CSV
+            using var writer = new StreamWriter(outputFile); // Opens stream to the output CSV file
+            using var csvOut = new CsvWriter(writer, CultureInfo.InvariantCulture); // Writes to the file
+            csvOut.WriteRecords(outputRecords);
+        }
+    }
+}
 
 
 
