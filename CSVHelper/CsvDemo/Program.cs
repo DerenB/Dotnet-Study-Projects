09432c325812dda04df691984dd06ab6e484faed
Deren B <47374311+DerenB@users.noreply.github.com>
2023-08-17 23:33:32 -0400

    CSV Helper

---
+++ CSVHelper/CsvDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using CsvDemo;
+using CsvHelper;
+
+string inputFile = @"C:\Users\dboze\Documents\LocalRepo\Dotnet\Dotnet-Study-Projects\CSVHelper\people-100.csv";
+string outputFile = @"C:\Users\dboze\Documents\LocalRepo\Dotnet\Dotnet-Study-Projects\CSVHelper\filtered-people.csv";
+
+List<PersonModel> outputRecords = new();
+
+// Read CSV file
+using var reader = new StreamReader(inputFile);
+using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+
+
+
+
+
+
+
+
+
+
+
+
